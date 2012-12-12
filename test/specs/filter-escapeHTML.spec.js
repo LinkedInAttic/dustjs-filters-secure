98eb0502396d2c90188196c4dc5cd7fd97f9e101
Jimmy Chan <jchan@linkedin.com>
2012-12-12 09:42:37 -0800

    derp derp working again

--- test/specs/filter-escapeHTML.spec.js
+++ test/specs/filter-escapeHTML.spec.js
@@ -6,13 +6,11 @@ var aStrings = require('../setup/simple-strings'),
     testArray = aHTMLStrings.concat(aBadHTMLStrings);
 
 
-console.log(testArray);
-
 describe('dust escapeHtml |h filters works', function() {
   it('should return a string', function(){
     var before, after;
     for (var i=0, len=testArray.length; i<len; i++){
-      before = aHTMLStrings[i];
+      before = testArray[i];
       after = dustFilters.h(before);
       expect(typeof after).toEqual('string');
     }
@@ -20,27 +18,27 @@ describe('dust escapeHtml |h filters works', function() {
   it('should return a string that is around the same length or greater than the original', function(){
     var before, after;
     for (var i=0, len=testArray.length; i<len; i++){
-      before = aHTMLStrings[i];
+      before = testArray[i];
       after = dustFilters.h(before);
       expect(typeof after).toEqual('string');
       expect(after.length).toBeGreaterThan(before.length);
-      expect(after.length).toBeLessThan(before.length * 1.2);
-      // console.log(dustFilters.h(aHTMLStrings[i]));
+      expect(after.length).toBeLessThan(before.length * 3);
+      // console.log(dustFilters.h(testArray[i]));
     }
   });
   it('should not contain < > or double quotes', function(){
     for (var i=0, len=testArray.length; i<len; i++){
-      expect(dustFilters.h(aHTMLStrings[i])).not.toMatch('/[<>"]/');
-      // console.log(dustFilters.h(aHTMLStrings[i]));
+      expect(dustFilters.h(testArray[i])).not.toMatch('/[<>"]/');
+      // console.log(dustFilters.h(testArray[i]));
     }
   });
   it('should have the custom escapeHtml method', function(){
     expect(oldFilters.h).not.toBe(dustFilters.h);
   });
   it('should be backwards compatible', function(){
-    // for (var i=0, len=aHTMLStrings.length; i<len; i++){
-      // console.log(dustFilters.h(aHTMLStrings[i]), oldFilters.h(aHTMLStrings[i]));
-      // expect(dustFilters.h(aHTMLStrings[i])).toEqual(oldFilters.h(aHTMLStrings[i]));
+    // for (var i=0, len=testArray.length; i<len; i++){
+      // console.log(dustFilters.h(testArray[i]), oldFilters.h(testArray[i]));
+      // expect(dustFilters.h(testArray[i])).toEqual(oldFilters.h(testArray[i]));
     // }
   });
 });
