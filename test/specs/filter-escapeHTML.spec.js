57fdfd40c97fabdae0896abeb87da7e7864da081
Jimmy Chan <jchan@linkedin.com>
2012-12-12 09:14:09 -0800

    wrote tests that made escape HTML fail

--- test/specs/filter-escapeHTML.spec.js
+++ test/specs/filter-escapeHTML.spec.js
@@ -9,10 +9,29 @@ var aStrings = require('../setup/simple-strings'),
 console.log(testArray);
 
 describe('dust escapeHtml |h filters works', function() {
+  it('should return a string', function(){
+    var before, after;
+    for (var i=0, len=testArray.length; i<len; i++){
+      before = aHTMLStrings[i];
+      after = dustFilters.h(before);
+      expect(typeof after).toEqual('string');
+    }
+  });
+  it('should return a string that is around the same length or greater than the original', function(){
+    var before, after;
+    for (var i=0, len=testArray.length; i<len; i++){
+      before = aHTMLStrings[i];
+      after = dustFilters.h(before);
+      expect(typeof after).toEqual('string');
+      expect(after.length).toBeGreaterThan(before.length);
+      expect(after.length).toBeLessThan(before.length * 1.2);
+      // console.log(dustFilters.h(aHTMLStrings[i]));
+    }
+  });
   it('should not contain < > or double quotes', function(){
     for (var i=0, len=testArray.length; i<len; i++){
       expect(dustFilters.h(aHTMLStrings[i])).not.toMatch('/[<>"]/');
-      console.log(dustFilters.h(aHTMLStrings[i]));
+      // console.log(dustFilters.h(aHTMLStrings[i]));
     }
   });
   it('should have the custom escapeHtml method', function(){
