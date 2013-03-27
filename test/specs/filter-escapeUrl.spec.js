ffbb28df4ef52382ae4e5dd7a847ab252955043b
Jimmy Chan <jchan@linkedin.com>
2013-03-27 12:09:39 -0700

    remove url filter

--- test/specs/filter-escapeUrl.spec.js
+++ test/specs/filter-escapeUrl.spec.js
@@ -2,14 +2,14 @@ var aStrings = require('../setup/simple-strings'),
     aBadStrings = require('../setup/bad-strings'),
     aUrls = require('../setup/simple-urls'),
     aBadUrls = require('../setup/bad-urls'),
-    customEscapeUrl = dustFilters.url,
+    // customEscapeUrl = dustFilters.url,
     testStrings = aStrings.concat(aUrls).concat(aBadStrings).concat(aBadUrls);
 
 
 describe('dust escapeUrl |url filters works', function() {
-  it('should not contain spaces, <, > or semicolons', function(){
-    for (var i=0, len=testStrings.length; i<len; i++){
-      expect(customEscapeUrl(testStrings[i])).not.toMatch('/[ <>;]/');
-    }
-  });
+  // it('should not contain spaces, <, > or semicolons', function(){
+    // for (var i=0, len=testStrings.length; i<len; i++){
+      // expect(customEscapeUrl(testStrings[i])).not.toMatch('/[ <>;]/');
+    // }
+  // });
 });
